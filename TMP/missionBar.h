e325b1d8966c32398c89f4a0428c2110c41f7543
yue9944882 <291271447@qq.com>
2015-12-17 21:07:44 +0800

    Deprecated codes need cleaning

--- TMP/missionBar.h
+++
@@ -1,8 +0,0 @@
-/****************************************************************************
-** Form interface generated from reading ui file 'missionBar.ui'
-**
-** Created by User Interface Compiler
-**
-** WARNING! All changes made in this file will be lost!
-****************************************************************************/
-
--- TMP/newDlg.h
+++
@@ -1,8 +0,0 @@
-/****************************************************************************
-** Form interface generated from reading ui file 'newDlg.ui'
-**
-** Created by User Interface Compiler
-**
-** WARNING! All changes made in this file will be lost!
-****************************************************************************/
-
